e44cd235bced69740201d76e1a4fe4155af0629e
anh <loatothe1@gmail.com>
2023-10-23 23:19:08 +0700

    ủa

--- src/Page/admin/ComOrderTableRequest/OrderCanceled.jsx
+++ src/Page/admin/ComOrderTableRequest/OrderCanceled.jsx
@@ -114,10 +114,11 @@ export default function OrderCanceled() {
     };
     function formatCurrency(number) {
         // Sử dụng hàm toLocaleString() để định dạng số thành chuỗi với ngăn cách hàng nghìn và mặc định là USD.
-        return number.toLocaleString('en-US', {
-            style: 'currency',
-            currency: 'VND',
-        });
+        return number
+        // .toLocaleString('en-US', {
+        //     style: 'currency',
+        //     currency: 'VND',
+        // });
     }
     const CreateProductMessenger = yup.object({
 
