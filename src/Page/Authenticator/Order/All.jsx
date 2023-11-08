9ee5dae9cd362668f0d73937dcd63f61557c6de5
toannv72 <64313624+toannv72@users.noreply.github.com>
2023-11-08 12:49:32 +0700

    up

--- src/Page/Authenticator/Order/All.jsx
+++ src/Page/Authenticator/Order/All.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { textApp } from "../../../TextContent/textApp";
 import { getData } from '../../../api/api';
 
-export default function Pending() {
+export default function Pending({activeTab}) {
   const [order, setOrder] = useState([]);
   const [products, setProducts] = useState([]);
 
@@ -28,7 +28,7 @@ export default function Pending() {
     
         });
     // }
-  }, []);
+  }, [activeTab]);
 
   const getProductById = (productId) => {
     // Tìm sản phẩm theo ID trong danh sách sản phẩm
